4356927678ad16ca99e1e36f679356994f96d289
Stephen Afam-Osemene <me@stephenafamo.com>
2023-11-10 21:38:58 +0000

    Initial commit

---
+++ argon2.go
@@ -0,0 +1,38 @@
+package authbossargon2
+
+import (
+	"errors"
+
+	"github.com/alexedwards/argon2id"
+)
+
+var ErrMismatchedHashAndPassword = errors.New("hashedPassword is not the hash of the given password")
+
+// NewArgon2Hasher returns a hasher that uses the argon2id hashing algorithm.
+// It is compatible with the `authboss.Hasher` interface
+func NewArgon2Hasher(params *argon2id.Params) *argon2Hasher {
+	if params == nil {
+		params = argon2id.DefaultParams
+	}
+	return &argon2Hasher{params: *params}
+}
+
+type argon2Hasher struct {
+	params argon2id.Params
+}
+
+func (h argon2Hasher) GenerateHash(password string) (string, error) {
+	return argon2id.CreateHash(password, argon2id.DefaultParams)
+}
+
+func (h *argon2Hasher) CompareHashAndPassword(hashedPassword, password string) error {
+	matched, err := argon2id.ComparePasswordAndHash(password, hashedPassword)
+	if err != nil {
+		return err
+	}
+	if !matched {
+		return ErrMismatchedHashAndPassword
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/stephenafamo/authboss-argon2
+
+go 1.21.3
+
+require github.com/alexedwards/argon2id v1.0.0
+
+require (
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
